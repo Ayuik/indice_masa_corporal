108b18751ece67b7543bbdedb34912b2e32c1e6e
Ayuik <marcosayelen@gmail.com>
2025-02-06 08:40:01 +0100

    BodyMassIndex and its test finished. No rounding necessary.

--- src/main/java/dev/ayelen/indice_masa_corporal/BodyMassIndex.java
+++ src/main/java/dev/ayelen/indice_masa_corporal/BodyMassIndex.java
@@ -5,12 +5,4 @@ public class BodyMassIndex {
     public double calculateBMI(double wheight, double height) {
          return wheight / Math.pow(height, 2);
     }
-
-    /* public double roundResult (double result) {
-        if (result == Math.floor(result)) {
-            return (double)Math.round(result * 10d) / 10d;
-        } else {
-        return result;
-        }    } */
-
 }
