e9432de1700733daa115aa721b84dbd54f8b8355
Ayuik <marcosayelen@gmail.com>
2025-02-06 09:54:05 +0100

    testReturnDelgadezSevera fails

---
+++ src/main/java/dev/ayelen/indice_masa_corporal/ResultMessage.java
@@ -0,0 +1,5 @@
+package dev.ayelen.indice_masa_corporal;
+
+public class ResultMessage {
+    
+}
---
+++ src/test/java/dev/ayelen/indice_masa_corporal/ResultMessageTest.java
@@ -0,0 +1,20 @@
+package dev.ayelen.indice_masa_corporal;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+
+public class ResultMessageTest {
+
+    @Test
+    @DisplayName("Returns delgadez severa if imc < 16")
+    void testReturnDelgadezSevera() {
+        double imc = 11;
+        ResultMessage resultMessage = new ResultMessage();
+        String message = resultMessage.returnDelgadezSevera();
+        assertThat(message, is("Delgadez severa"));
+    }
+    
+}
