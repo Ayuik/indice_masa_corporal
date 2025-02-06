8924cfe30059ad88b6792feacb788d3932a3f8e6
Ayuik <marcosayelen@gmail.com>
2025-02-06 09:15:00 +0100

    test getWheight fails

--- src/test/java/dev/ayelen/indice_masa_corporal/PersonTest.java
+++ src/test/java/dev/ayelen/indice_masa_corporal/PersonTest.java
@@ -16,5 +16,13 @@ public class PersonTest {
         double result = person.getHeight();
         assertEquals(1.67, result);
     }
+
+    @Test
+    @DisplayName("Should get wheight")
+    void testGetwheight() {
+        Person person = new Person(53, 0)
+        double result = person.getWheight();
+        assertEquals(53, result);
+    }
 }
 
